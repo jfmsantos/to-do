f9d9450622ea17ea0d4368ba35982b98a40d8ee7
Fabrício Menezes <jfmsantos@outlook.com>
2024-03-05 02:20:20 -0300

    # Adicionar - Validacao dos campos obrigatorios

---
+++ src/middlewares/TaskValidation.js
@@ -0,0 +1,38 @@
+const TaskModel = require('../model/TaskModel');
+const { isPast } = require('data-fns');
+
+const TaskValidation = async (req, res, next) => {
+
+    const { macaddress, type, title, description, when } = req.body;
+
+    if (!macaddress)
+        return res.status(400).json({ error: 'macaddress é obrigatório' });
+    else if (!type)
+        return res.status(400).json({ error: 'tipo é obrigatório' });
+    else if (!title)
+        return res.status(400).json({ error: 'título é obrigatório' });
+    else if (!description)
+        return res.status(400).json({ error: 'descrição é obrigatória' });
+    else if (!when)
+        return res.status(400).json({ error: 'Data e Hora são obrigatórios' });
+    /*else if (isPast(new Date(when)))
+        return res.status(400).json({ error: 'informe uma data e hora futura' });*/
+    else {
+        let exists;
+
+        exists = await TaskModel.findOne(
+            {
+                'when': { '$eq': new Date(when) },
+                'macaddress': { '$in': macaddress }
+            }
+        );
+        if (exists) {
+            return res.status(400).json({ error: 'já existe uma tarefa para esse dia e horário' });
+        }
+
+        next();
+    }
+
+}
+
+module.exports = TaskValidation;
