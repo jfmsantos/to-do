0c4c45360ef99e17e4a07051424803568057d8bb
Fabrício Menezes <jfmsantos@outlook.com>
2024-03-06 00:29:04 -0300

    # Adicionar - Listar todas as tarefas.

--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -3,9 +3,12 @@ const router = express.Router();
 
 const TaskController = require("../controller/TaskController");
 const TaskValidation = require("../middlewares/TaskValidation");
+const MacaddressValidation = require("../middlewares/macaddressValidation");
 
 router.post("/", TaskValidation, TaskController.create);
 
 router.put("/:id", TaskValidation, TaskController.update);
+
+router.get("/filter/all", MacaddressValidation, TaskController.all);
 
 module.exports = router;
