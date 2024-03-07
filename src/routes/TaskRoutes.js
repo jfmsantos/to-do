f84b5623a7ce278b0c69d06d07650f3e4193fc98
Fabrício Menezes <jfmsantos@outlook.com>
2024-03-06 22:59:30 -0300

    # Adicionar - Exibir tarefas atrazadas

--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -7,9 +7,10 @@ const MacaddressValidation = require("../middlewares/macaddressValidation");
 
 router.post("/", TaskValidation, TaskController.create);
 router.put("/:id", TaskValidation, TaskController.update);
-router.get("/:id", TaskController.show);
-router.delete("/:id", TaskController.delete);
-router.get("/filter/all", MacaddressValidation, TaskController.all);
 router.put("/:id/:done", TaskController.done);
+router.delete("/:id", TaskController.delete);
+router.get("/:id", TaskController.show);
+router.get("/filter/all", MacaddressValidation, TaskController.all);
+router.get("/filter/late", MacaddressValidation, TaskController.late);
 
 module.exports = router;
