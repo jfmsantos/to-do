403402f400b3d4539367ec5a3e92c95e98328f03
Fabrício Menezes <jfmsantos@outlook.com>
2024-03-09 22:41:52 -0300

    # Adicionar - Filtro tarefa da semana

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -1,6 +1,6 @@
 const { response } = require("express");
 const TaskModel = require("../model/TaskModel");
-const { startOfDay, endOfDay } = require("date-fns");
+const { startOfDay, endOfDay, startOfWeek, endOfWeek } = require("date-fns");
 
 const current = new Date();
 
@@ -100,7 +100,24 @@ class TaskController {
             macaddress: { $in: req.body.macaddress },
             when: {
                 $gte: startOfDay(current),
-                $lt: endOfDay(current),
+                $lte: endOfDay(current),
+            },
+        })
+            .sort("when")
+            .then((response) => {
+                return res.status(200).json(response);
+            })
+            .catch((error) => {
+                return res.status(500).json(error);
+            });
+    }
+
+    async week(req, res) {
+        await TaskModel.find({
+            macaddress: { $in: req.body.macaddress },
+            when: {
+                $gte: startOfWeek(current),
+                $lte: endOfWeek(current),
             },
         })
             .sort("when")
