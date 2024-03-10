2470942b70d771942e887be30d7fd11dd3a64186
Fabrício Menezes <jfmsantos@outlook.com>
2024-03-09 22:47:29 -0300

    # Adicionar - Filtro tarefa dp mes

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -1,6 +1,13 @@
 const { response } = require("express");
 const TaskModel = require("../model/TaskModel");
-const { startOfDay, endOfDay, startOfWeek, endOfWeek } = require("date-fns");
+const {
+    startOfDay,
+    endOfDay,
+    startOfWeek,
+    endOfWeek,
+    startOfMonth,
+    endOfMonth,
+} = require("date-fns");
 
 const current = new Date();
 
@@ -118,6 +125,23 @@ class TaskController {
             when: {
                 $gte: startOfWeek(current),
                 $lte: endOfWeek(current),
+            },
+        })
+            .sort("when")
+            .then((response) => {
+                return res.status(200).json(response);
+            })
+            .catch((error) => {
+                return res.status(500).json(error);
+            });
+    }
+
+    async month(req, res) {
+        await TaskModel.find({
+            macaddress: { $in: req.body.macaddress },
+            when: {
+                $gte: startOfMonth(current),
+                $lte: endOfMonth(current),
             },
         })
             .sort("when")
