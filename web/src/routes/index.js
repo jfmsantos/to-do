b142e0137b425b4e82c34797353f71a967d86706
Fabrício Menezes <jfmsantos@outlook.com>
2024-03-22 01:06:01 -0300

    # Adicionar - Rota Qrdcode.

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "../views/Home";
 import Task from "../views/Task";
+import Qrcode from "../views/QrCode";
 
 export default function Rotas() {
     return (
@@ -11,6 +12,7 @@ export default function Rotas() {
                 <Route path="/" exact Component={Home} />
                 <Route path="/task" exact Component={Task} />
                 <Route path="/task/:id" exact Component={Task} />
+                <Route path="/qrcode" exact Component={Qrcode} />
             </Routes>
         </BrowserRouter>
     );
