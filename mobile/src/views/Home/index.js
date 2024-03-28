8d6a1fb807192958eec64c276b6b3c6e80a70dcc
Fabrício Menezes <jfmsantos@outlook.com>
2024-03-27 22:48:30 -0300

    # Adicionar - Footer dinamico

--- mobile/src/views/Home/index.js
+++ mobile/src/views/Home/index.js
@@ -5,11 +5,13 @@ import styles from "./styles";
 
 //Componentes
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 export default function Home() {
     return (
         <View style={styles.container}>
             <Header showNotification={true} showBack={false} />
+            <Footer icon={"add"}></Footer>
         </View>
     );
 }
